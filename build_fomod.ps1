1441a51a27ac7c771f54269ee0d624f07951a44c
strangerism <massimovirgilio@hotmail.com>
2024-04-28 23:31:11 +0200

    first commit

---
+++ build_fomod.ps1
@@ -0,0 +1,29 @@
+
+
+function CreateBuildFolders {
+    
+    New-Item -Path "build" -ItemType Directory | Out-Null
+
+    New-Item -Path "build/NPE" -ItemType Directory | Out-Null
+    
+}
+
+function BuildMod {
+
+    $target = "build/NPE"
+    Copy-Item -Recurse -Force -Path ".\fomod", ".\Main", ".\Module" -Destination $target -Exclude .bak
+
+    # Copy-Item -Force -Path ".\doc\NPE.pdf" -Destination $target
+}
+
+CreateBuildFolders
+BuildMod
+
+$compress = @{
+    Path = "build/NPE" 
+    CompressionLevel = "Fastest"
+    DestinationPath = "release/NPE.zip"
+}
+Compress-Archive @compress -Force
+
+Remove-Item -Force -Recurse -Path "./build"
