dc663ef19775b7aa8f12459ccd444da3e65d96cd
strangerism <massimovirgilio@hotmail.com>
2024-05-09 18:47:05 +0200

    added configurable detector api, scanlines on portraits, uses default dialog if not declared in set

---
+++ deploy.ps1
@@ -0,0 +1,5 @@
+
+
+$dest = "D:\games\GAMMA\GAMMA RC3\mods\New Player Experience"
+
+Copy-Item -Path ".\Main\gamedata", ".\Module\gamedata", ".\Examples\NPE Codec\gamedata" -Destination $dest -Exclude .gitignore, .bak -Force -Recurse
